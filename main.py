1ede40960e78e0537899c4c4f4b3551e1ccbfb9a
iqboljon7 <rsfordwyn@gmail.com>
2023-08-09 17:13:38 +0500

    chekpoin few

--- main.py
+++ main.py
@@ -17,7 +17,9 @@ dispatcher = Dispatcher()
 dp = Router()
 dispatcher.include_router(dp)
 
-
+@dp.message(Command("nth"))
+async def cmd_start(message: types.Message):
+    await message.answer("nothing")
 
 @dp.message(Command("start"))
 async def cmd_start(message: types.Message, state: FSMContext):
@@ -25,21 +27,38 @@ async def cmd_start(message: types.Message, state: FSMContext):
     await message.answer("Send the date of birth of the person whose information you want to find in the format day.month.year")
     await state.set_state(req.brdate)
 
-@dp.message(Command("nth"))
-async def cmd_start(message: types.Message):
-    await message.answer("nothing")
 @dp.message(req.brdate)
 async def birthday(msg: types.Message,state: FSMContext):
-    await state.update_data(brdate=msg.text)
-    await msg.answer("Now enter your passport number and serial number. For example: AD12345667")
-    await state.set_state(req.ser_num)
+    m=msg.text
+    if m[2]==m[5]==".":
+        a,b,c = msg.text.split(".")
+        try:
+            if int(a) and int(b) and int(c):
+                await state.update_data(brdate=msg.text)
+                await msg.answer("Now enter your passport number and serial number. For example: AD12345667")
+                await state.set_state(req.ser_num)
+            else:
+                await msg.answer("you entered information in wrong format")
+        except:
+            await msg.answer("you entered information in wrong format")
+    else:
+        await msg.answer("you entered information in wrong format")
 
 @dp.message(req.ser_num)
 async def serial_number(msg: types.Message, state: FSMContext):
-    await state.update_data(ser_num=msg.text)
-    data = await state.get_data()
-    await msg.answer(rqs(data['brdate'],data['ser_num'][:2], data['ser_num'][2:]))
-
+    xabar=msg.text
+    if xabar[0]=="A" and len(xabar)==9:
+        try:
+            if int(xabar[2:]):
+                await state.update_data(ser_num=msg.text)
+                data = await state.get_data()
+                await msg.answer(rqs(data['brdate'],data['ser_num'][:2], data['ser_num'][2:]))
+            else:
+                await msg.answer("you entered information in wrong format")
+        except:
+            await msg.answer("you entered information in wrong format")
+    else:
+        await msg.answer("you entered information in wrong format")
 async def main():
     await dispatcher.start_polling(bot)
 
